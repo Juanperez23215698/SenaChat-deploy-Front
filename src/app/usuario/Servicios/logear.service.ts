8c41ed36503744114427d3f32bd6435afeadcc72
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2024-03-22 11:25:32 -0500

    Update logear.service.ts

--- src/app/usuario/Servicios/logear.service.ts
+++ src/app/usuario/Servicios/logear.service.ts
@@ -6,7 +6,7 @@ import { ChatService } from './chat.service';
   providedIn: 'root'
 })
 export class LogearService {
-  url = "mysql://u4u0okcmyjmw5reb:ZhHurKTg3hCg40ckVHSt@b3j38zupvodu63t4w42n-mysql.services.clever-cloud.com:3306/b3j38zupvodu63t4w42n";
+  url = "senachat-deploy-db.railway.internal";
   constructor(private http: HttpClient, private chat: ChatService) { }
   buscarDatos(datos: any) {
     return this.http.post(`${this.url}/usuario/login`, datos);
