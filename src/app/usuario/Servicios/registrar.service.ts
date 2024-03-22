06ab70a768255066bf01725cd8c8cbe5abc7e3a0
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2024-03-21 22:39:41 -0500

    Update registrar.service.ts

--- src/app/usuario/Servicios/registrar.service.ts
+++ src/app/usuario/Servicios/registrar.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class RegistrarService {
-  url = "http://localhost:3000";
+  url = "mysql://u4u0okcmyjmw5reb:ZhHurKTg3hCg40ckVHSt@b3j38zupvodu63t4w42n-mysql.services.clever-cloud.com:3306/b3j38zupvodu63t4w42n";
   constructor(private http: HttpClient) { }
   enviarDatos(datos: any) {
     return this.http.post<string>(`${this.url}/usuario/registrar`, datos);
